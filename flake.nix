3541afb89a152778185c543ca50b7768d08736a4
Ben C <bwc9876@gmail.com>
2024-02-01 22:01:22 -0500

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.rustPlatform.buildRustPackage {
+        pname = "gh-grader-preview";
+        version = "0.1.0";
+        src = ./.;
+        cargoLock = {
+            lockFile = ./Cargo.lock;
+        };
+        doCheck = false;
+        meta = with nixpkgs.legacyPackages.x86_64-linux.lib; {
+            description = "CLI version of the mod manager for Outer Wilds Mod Loader";
+            homepage = "https://github.com/Bwc9876/gh-grader-preview";
+            mainProgram = "gh-grader-preview";
+            license = licenses.mit;
+        };
+    };
+  };
+}
